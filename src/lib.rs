826d930fc01fdf744ac3bbebc8c8e1cd1ffc523e
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-12-08 14:04:01 -0600

    fix exchange bind still not able to catch ctrl-c

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,12 @@ use std::cell::RefCell;
 use std::fs::File;
 use std::io::Read;
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time;
 use amqp::{Session, Channel, Table, Basic, protocol};
 use serde_json::Value;
 
+extern crate ctrlc;
 extern crate toml;
 extern crate amqp;
 extern crate serde;
@@ -31,6 +32,25 @@ struct Config {
     _type: String,
     name: String,
     vhost: String,
+}
+
+#[derive(Debug, Serialize)]
+struct VimDriverDeregisterRequest {
+    #[serde(rename = "username")]
+    usr: String,
+    #[serde(rename = "password")]
+    pwd: String,
+    action: String,
+}
+
+impl VimDriverDeregisterRequest {
+    fn new(usr: String, pwd: String) -> VimDriverDeregisterRequest {
+        VimDriverDeregisterRequest {
+            usr: usr,
+            pwd: pwd,
+            action: String::from("deregister"),
+        }
+    }
 }
 
 #[derive(Debug, Serialize)]
@@ -69,19 +89,24 @@ struct OpenBatonPluginMessage {
 fn parse(path: &str) -> Config {
     let mut file_content = String::new();
     let mut f = File::open(path).expect("Unable to open file");
-    f.read_to_string(&mut file_content).expect("Unable to read string");
+    f.read_to_string(&mut file_content).expect(
+        "Unable to read string",
+    );
     info!("{}", file_content);
     let decoded: Config = toml::from_str(&file_content[..]).unwrap();
     decoded
 }
 
 fn get_user_and_pwd(config: &Config) -> (String, String) {
-    let amqp_uri = &format!("amqp://{}:{}@{}:{}/{}",
-                            config.username,
-                            config.password,
-                            config.broker_ip,
-                            config.broker_port,
-                            config.vhost)[..];
+    let amqp_uri = &format!(
+        "amqp://{}:{}@{}:{}/{}",
+        config.username,
+        config.password,
+        config.broker_ip,
+        config.broker_port,
+        config.vhost
+    )
+        [..];
     info!("Amqp URI: {}", amqp_uri);
     let mut session = Session::open_url(amqp_uri).unwrap();
     debug!("Opened Session");
@@ -97,7 +122,8 @@ fn get_user_and_pwd(config: &Config) -> (String, String) {
 
     // queue: &str, passive: bool, durable: bool, exclusive: bool, auto_delete: bool, nowait: bool,
     // arguments: Table
-    let queue_declare = channel.queue_declare("", false, false, true, true, false, Table::new())
+    let queue_declare = channel
+        .queue_declare("", false, false, true, true, false, Table::new())
         .unwrap();
 
     debug!("Queue declare: {:?}", queue_declare);
@@ -122,26 +148,32 @@ fn get_user_and_pwd(config: &Config) -> (String, String) {
         tx.send(rc.password).unwrap();
     };
 
-    channel.basic_consume(closure_consumer,
-                          queue_name.to_string(),
-                          "".to_string(),
-                          false,
-                          false,
-                          false,
-                          false,
-                          Table::new())
+    channel
+        .basic_consume(
+            closure_consumer,
+            queue_name.to_string(),
+            "".to_string(),
+            false,
+            false,
+            false,
+            false,
+            Table::new(),
+        )
         .unwrap();
 
-    channel.basic_publish("",
-                          "nfvo.manager.handling",
-                          true,
-                          false,
-                          protocol::basic::BasicProperties {
-                              reply_to: Some(queue_name.to_string()),
-                              content_type: Some("text".to_string()),
-                              ..Default::default()
-                          },
-                          register_msg_json.as_bytes().to_vec())
+    channel
+        .basic_publish(
+            "",
+            "nfvo.manager.handling",
+            true,
+            false,
+            protocol::basic::BasicProperties {
+                reply_to: Some(queue_name.to_string()),
+                content_type: Some("text".to_string()),
+                ..Default::default()
+            },
+            register_msg_json.as_bytes().to_vec(),
+        )
         .unwrap();
 
     // let start_channel = Arc::clone(&channel);
@@ -160,20 +192,27 @@ fn get_user_and_pwd(config: &Config) -> (String, String) {
     return (username, password);
 }
 
-#[allow(dead_code)]
-pub fn start_instances<V: openbaton::VimDriver + 'static>(num_of_instances: usize, vim_driver: V, vim_type: &str, vim_name: &str) {
+pub fn start_instances<V: openbaton::VimDriver + 'static>(
+    num_of_instances: usize,
+    vim_driver: V,
+    vim_type: &str,
+    vim_name: &str,
+) {
     info!("Starting {} instances", num_of_instances);
+
     let config: Config = parse("config.toml");
     let (usr, pwd) = get_user_and_pwd(&config);
     info!("Got  usr:{} anf pwd:{} instances", usr, pwd);
 
-
-    let amqp_uri = &format!("amqp://{}:{}@{}:{}/{}",
-                            usr,
-                            pwd,
-                            config.broker_ip,
-                            config.broker_port,
-                            config.vhost)[..];
+    let amqp_uri = &format!(
+        "amqp://{}:{}@{}:{}/{}",
+        usr,
+        pwd,
+        config.broker_ip,
+        config.broker_port,
+        config.vhost
+    )
+        [..];
     info!("Amqp URI: {}", amqp_uri);
     let mut session = Session::open_url(amqp_uri).unwrap();
     debug!("Opened Session");
@@ -181,7 +220,16 @@ pub fn start_instances<V: openbaton::VimDriver + 'static>(num_of_instances: usiz
     let mut channel = session.open_channel(1).unwrap();
     debug!("Opened Channel");
 
-    let queue_declare = channel.queue_declare(format!("vim-drivers.{}.{}", vim_type, vim_name), false, false, true, true, false, Table::new())
+    let queue_declare = channel
+        .queue_declare(
+            format!("vim-drivers.{}.{}", vim_type, vim_name),
+            false,
+            false,
+            false,
+            true,
+            false,
+            Table::new(),
+        )
         .unwrap();
 
     debug!("Queue declare: {:?}", queue_declare);
@@ -189,16 +237,23 @@ pub fn start_instances<V: openbaton::VimDriver + 'static>(num_of_instances: usiz
     let queue_name = &queue_declare.queue[..];
     let exchange_name = "openbaton-exchange";
     debug!("Declaring exchange...");
-    let exchange_declare1 = channel.exchange_declare(exchange_name, "topic",true, true, false, false, false, Table::new());
+    let exchange_declare1 = channel.exchange_declare(
+        exchange_name,
+        "topic",
+        true,
+        true,
+        false,
+        false,
+        false,
+        Table::new(),
+    );
     debug!("Exchange declare: {:?}", exchange_declare1);
     debug!("Binding {} with {}", queue_name, exchange_name);
-    match channel.exchange_bind(queue_name, exchange_name, queue_name, Table::new()) {
-        Err(err) => {
-            error!("Error binding: {:?}", err);
-            std::process::exit(32);
-        },
-        Ok(_) => {},
-    };
+    // let dur = std::time::Duration::from_secs(100);
+    // std::thread::sleep(dur);
+    channel
+        .queue_bind(queue_name, exchange_name, queue_name, false, Table::new())
+        .unwrap();
     debug!("Bind to exchange");
     channel.basic_prefetch(1).ok().expect("Failed to prefetch");
 
@@ -213,38 +268,74 @@ pub fn start_instances<V: openbaton::VimDriver + 'static>(num_of_instances: usiz
 
         match plugin_msg.method_name {
             MethodName::Refresh => {
-//                let res: <V as openbaton::VimDriver>::T;
-                let res = vim_driver.borrow_mut().refresh(serde_json::from_str(&plugin_msg.parameters[0][..]).unwrap());
+                //                let res: <V as openbaton::VimDriver>::T;
+                let res = vim_driver.borrow_mut().refresh(
+                    serde_json::from_str(
+                        &plugin_msg.parameters[0][..],
+                    ).unwrap(),
+                );
                 if let amqp::TableEntry::LongString(ref reply_to) = headers.headers.unwrap()["reply_to"] {
-                    let properties = protocol::basic::BasicProperties { content_type: Some("text".to_owned()), ..Default::default() };
-                    _chan.basic_publish(exchange_name,
-                                        &reply_to[..],
-                                        true,
-                                        false,
-                                        properties,
-                                        serde_json::to_string(&res).unwrap().into_bytes())
-                        .ok().expect("Failed publishing");
+                    let properties = protocol::basic::BasicProperties {
+                        content_type: Some("text".to_owned()),
+                        ..Default::default()
+                    };
+                    _chan
+                        .basic_publish(
+                            exchange_name,
+                            &reply_to[..],
+                            true,
+                            false,
+                            properties,
+                            serde_json::to_string(&res).unwrap().into_bytes(),
+                        )
+                        .ok()
+                        .expect("Failed publishing");
                 }
             }
         }
     };
 
 
-
-    channel.basic_consume(listener_closure,
-                          queue_name,
-                          "",
-                          false,
-                          false,
-                          false,
-                          false,
-                          Table::new())
+    channel
+        .basic_consume(
+            listener_closure,
+            queue_name,
+            "",
+            false,
+            false,
+            false,
+            false,
+            Table::new(),
+        )
         .unwrap();
 
-    channel.start_consuming();
+    let arc_chan = Arc::new(Mutex::new(channel));
+    let chan = Arc::clone(&arc_chan);
 
-    channel.close(200, "Bye").unwrap();
-    session.close(200, "Good Bye");
+    let arc_sess = Arc::new(Mutex::new(session));
+    let sess = Arc::clone(&arc_sess);
+
+    let arc_usr = Arc::new(usr);
+    let arc_pwd = Arc::new(pwd);
+
+    ctrlc::set_handler(move || {
+        let deregister_msg_json = serde_json::to_string(&VimDriverDeregisterRequest::new(String::from(arc_usr.as_str()), String::from(arc_pwd.as_str()))).unwrap();
+        let mut mutex_chan = chan.lock().unwrap();
+        debug!("Unregistering: {}", deregister_msg_json);
+        mutex_chan.basic_publish(
+            "",
+            "nfvo.manager.handling",
+            true,
+            false,
+            protocol::basic::BasicProperties { ..Default::default() },
+            deregister_msg_json.as_bytes().to_vec()
+        ).unwrap();
+        mutex_chan.close(22, "bye").unwrap();
+        sess.lock().unwrap().close(200, "Good Bye");
+    }).expect("Error setting Ctrl-C handler");
+    debug!("Correctly set ctrl-c handler");
+    let chan1 = Arc::clone(&arc_chan);
+    chan1.lock().unwrap().start_consuming();
 }
 
 
